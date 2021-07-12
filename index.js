689ec85b177130c926222767aa1d3878ff417880
Michelle Oakley <michellecoder38@gmail.com>
2021-07-12 08:57:29 -0700

    added and deleted functions for a path

--- index.js
+++ index.js
@@ -1,70 +1,83 @@
 // TODO: Include packages needed for this application
-const inquirer = require('inquirer');
-const generateMarkdown = require('./utils/generateMarkdown');
-const fs = require('fs')
+const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown");
+const fs = require("fs");
+const path = require("path");
 
 // TODO: Create an array of questions for user input
+
 const questions = [{
-        type: 'input',
-        message: 'What is the title of your README Project?',
-        name: 'title',
+        type: "input",
+        message: "What is your email?",
+        name: "email",
     },
 
     {
-        type: 'input',
-        message: 'What is the description of your Project?',
-        name: 'description',
+        type: "input",
+        message: "What is your github username?",
+        name: "github_username",
     },
 
     {
-        type: 'list',
-        message: 'What license do you want to use?',
-        name: 'license',
-        choices: ['MIT', 'Apache 2.0', 'IBM', 'Mozilla', 'None']
-    },
-
-
-    {
-        type: 'input',
-        message: 'How do you install your Project?',
-        name: 'install',
+        type: "input",
+        message: "What is the title of your README Project?",
+        name: "title",
     },
 
     {
-        type: 'input',
-        message: 'Waht is the proper use of this Project?',
-        name: 'usage',
+        type: "input",
+        message: "What is the description of your Project?",
+        name: "description",
     },
 
     {
-        type: 'input',
-        message: 'How do you contribute to this Project?',
-        name: 'contribution',
+        type: `list`,
+        message: `What license do you want to use?`,
+        name: "license",
+        choices: ["MIT", "Apache 2.0", "IBM", "GitHub", "None"],
     },
 
     {
-        type: 'input',
-        message: 'What test were perfomed on this Project?',
-        name: 'test',
-    }
+        type: "input",
+        message: "How do you install your Project?",
+        name: "install",
+    },
 
+    {
+        type: "input",
+        message: "What is the proper use of this Project?",
+        name: "usage",
+    },
+
+    {
+        type: "input",
+        message: "How do you contribute to this Project?",
+        name: "contribution",
+    },
+
+    {
+        type: "input",
+        message: "What test were perfomed on this Project?",
+        name: "test",
+    },
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {
-    return `# ${answers}`
+function writeToFile(fileName, answers) {
+    const data = generateMarkdown(answers);
+    fs.writeFile(fileName, data, (err) =>
+        err ? console.error(err) : console.log("Pass")
+    );
 }
 
 // TODO: Create a function to initialize app
 function init() {
-    inquirer.prompt(questions)
-        .then((answers) => {
-                console.log(answers);
-            },
-            fs.writeToFile('READMEgenerator.md', data), (err) => err ? console.error(err) : console.log('Pass')
-        )
-};
-
+    inquirer.prompt(questions).then((answers) => {
+        console.log(answers);
+        const fileName = path.join(__dirname, "README.md");
+        writeToFile(fileName, answers);
+    });
+}
 
 // Function call to initialize app
 init();
